33cf95e6276602fe021f8ba7f7c954fac9370a15
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-10-20 12:51:17 -0400

    Added nested resolvers

--- index.js
+++ index.js
@@ -8,31 +8,57 @@ const PORT = 4000
 const resolvers = {
   Query: {
     games() {
-      return db.games
+      return db.games;
     },
     game(parent, args, context) {
       return db.games.find((game) => {
-        return game.id === args.id;
+        return game.id === args.id
       });
     },
     reviews() {
-      return db.reviews
+      return db.reviews;
     },
     review(parent, args, context) {
       return db.reviews.find((review) => {
-        return review.id === args.id;
+        return review.id === args.id
       });
     },
     authors() {
-      return db.authors
+      return db.authors;
     },
     author(parent, args, context) {
       return db.authors.find((author) => {
-        return author.id === args.id;
+        return author.id === args.id
       });
-    }
-  }
-}
+    },
+  },
+  Game: {
+    reviews(parent, args, context) {
+      return db.reviews.filter((review) => {
+        return review.game_id === parent.id
+      });
+    },
+  },
+  Author: {
+    reviews(parent, args, context) {
+      return db.reviews.filter((review) => {
+        return review.author_id === parent.id
+      });
+    },
+  },
+  Review: {
+    author(parent, args, context) {
+      return db.authors.find((author) => {
+        return author.id === parent.author_id
+      });
+    },
+    game(parent, args, context) {
+      return db.games.find((game)=>{
+        return game.id === parent.game_id
+      })
+    },
+  },
+};
 
 
 // Server Setup
